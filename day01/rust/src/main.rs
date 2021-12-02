4b29f7a1b6e2d74cb2d0e18bd7be032b11ab35e6
Daniel Marks <86933755+MarksDaniel@users.noreply.github.com>
2021-12-02 00:23:51 +0000

    Update main.rs

--- day01/rust/src/main.rs
+++ day01/rust/src/main.rs
@@ -3,8 +3,10 @@ fn solve(depths: &[usize], window: usize) -> usize {
 }
 
 fn main() {
-    let depths: Vec<usize> = include_str!("../../input.txt").lines()
-                            .map(|line|line.parse::<usize>().unwrap()).collect::<Vec<usize>>();
+    let depths: Vec<usize> = include_str!("../../input.txt")
+                             .lines()
+                             .map(|line|line.parse::<usize>().unwrap())
+                             .collect::<Vec<usize>>();
     println!("{}", solve(&depths, 1)); // Part One
     println!("{}", solve(&depths, 3)); // Part Two 
 }
