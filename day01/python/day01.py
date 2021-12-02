a74f668c5bd1d5c168d95cfbcf70db971c73e0c3
Daniel Marks <86933755+MarksDaniel@users.noreply.github.com>
2021-12-02 00:22:29 +0000

    Update day01.py

--- day01/python/day01.py
+++ day01/python/day01.py
@@ -1,7 +1,7 @@
 def solve(depths, window):
     return sum(x < y for (x, y) in zip(depths, depths[window:]))
 
-with open("../input.txt")as file:
+with open("../input.txt") as file:
     depths = [int(n) for n in file.readlines()] 
     print(solve(depths, 1)); # Part One 
     print(solve(depths, 3)); # Part Two 
